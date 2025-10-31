389139be072ac77c1493d1464b57f2f20d5e36a1
Ari Piceno <a23300223@ceti.mx>
2025-10-31 01:12:35 -0600

    Add files via upload

---
+++ src/exam_palindromo/Exam_Palindromo.java
@@ -0,0 +1,11 @@
+
+package exam_palindromo;
+
+public class Exam_Palindromo {
+
+    public static void main(String[] args) {
+        Palindromo pal = new Palindromo();
+        pal.setVisible(true);
+    }
+    
+}
